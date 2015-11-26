7b9b56606c11f68cc87934dc223b702bb687d3d0
Keerthikan <kerat12@student.sdu.dk>
2015-11-26 14:26:29 +0100

    kiddi merge

--- sokoban_solver/src/main.cpp
+++ sokoban_solver/src/main.cpp
@@ -8,17 +8,7 @@ int main() {
 
   Sokoban map("../maps/map.txt");
   map.createGraph();
-  map.deadlockDetection();
-  Sokoban final("../maps/map.txt");
-  final.createGraph();
-  final.make_final();
-  cout << map.board[1][3]  << endl;
-  cout << "col: " << map.col << " row: " << map.row << endl;
-  wavefront wavefront(map.row, map.col, map.graph);
-  wavefront.get_wavefront('M');
-
-  a_star a_solver(map, final, &wavefront);
-  a_solver.solve();
+  
 
   return 0;
 }
